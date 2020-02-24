58a7362ffb51c9735edc3e4f86ec0f926c75c91e
Stephanie Zou <stephanie.zou@stern.nyu.edu>
2020-02-24 01:13:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,8 +23,12 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  hash[:key] += 1 
-  if 
+  
+  if hash[:key]
+    hash[:key] += 1 
+  else
+    puts "Key not found!"
+end
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
